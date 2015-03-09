efd5730906a0dbfceef2efc7ce78996038537339
Philip Ward <philipmward@hotmail.com>
2015-03-09 14:13:37 -0700

    welcome 1

---
+++ KendoNG/KendoNG/app/app.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    "use strict";
+
+    var app = angular.module("app", ["ngRoute", "kendo.directives"]);
+
+    app.config([
+        "$routeProvider", function ($routeProvider) {
+            $routeProvider
+                .when("/", { templateUrl: "app/welcome/welcome.html" })
+                .otherwise({ redirectTo: "/" });
+        }
+    ]);
+})();
---
+++ KendoNG/KendoNG/app/welcome/welcome.js
@@ -0,0 +1,21 @@
+﻿(function () {
+    "use strict";
+
+    angular.module("app").controller("welcome", [welcome]);
+
+    function welcome() {
+        var vm = this;
+
+        function activate() {
+            vm.computers = [
+                { name: "Macbook Pro", id: 1 },
+                { name: "Macbook Air", id: 2 },
+                { name: "Mac Pro", id: 3 }
+            ];
+        };
+
+        vm.activate = activate();
+
+        activate();
+    };
+})();
