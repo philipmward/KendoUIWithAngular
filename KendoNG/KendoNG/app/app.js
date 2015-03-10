2a231fc40cdfca1111f2dec59f0d72a39ad91815
Philip Ward <philipmward@hotmail.com>
2015-03-09 19:26:54 -0700

    commit

--- KendoNG/KendoNG/app/app.js
+++ KendoNG/KendoNG/app/app.js
@@ -10,6 +10,8 @@
                 .when("/welcome2", { templateUrl: "app/welcome/welcome2.html" })
                 .when("/welcome3", { templateUrl: "app/welcome/welcome3.html" })
                 .when("/modal", { templateUrl: "app/modal/modal.html" })
+                .when("/dataSource", { templateUrl: "app/dataSource/dataSource.html" })
+                .when("/order", { templateUrl: "app/order/order.html" })
                 .otherwise({ redirectTo: "/" });
         }
     ]);
---
+++ KendoNG/KendoNG/app/dataSource/dataSource.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    "use strict";
+
+    function dataSource() {
+        var vm = this;
+
+        function activate() {
+            vm.gridData = new kendo.data.ObservableArray([
+                { feature: "Disk", details: "256 MB" },
+                { feature: "RAM", details: "16 GB" },
+                { feature: "Monitor", details: "Thunderbolt" }
+            ]);
+
+            vm.gridColumns = [
+                { field: "feature", title: "Feature" },
+                { field: "details", title: "Details" }
+            ];
+
+            vm.update = function () {
+                vm.gridData[0].set("details", "1TB");
+            };
+        };
+
+        vm.activate = activate;
+        activate();
+    };
+
+    angular.module("app").controller("dataSource", [dataSource]);
+})();
